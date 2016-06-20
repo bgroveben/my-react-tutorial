4f0db99ae7898fdd5fe172804a2a54de141c52fc
bgroveben <bgroveben@gmail.com>
2016-06-20 04:27:03 -0700

    Update code for server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// https://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
+// Oh yeah, the ExpressJS docs helped too http://expressjs.com/en/starter/static-files.html
 var express = require('express');
 var app = express();
-var serveStatic = require('serve-static');
-app.use(serveStatic(__dirname)).listen(8080, function() {
+app.use(express.static(__dirname)).listen(8080, function() {
   console.log(('Server running on port 8080...'));
 });
