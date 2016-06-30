d625df2d227e5076a22e5aa3d660d8a9c94045e3
bgroveben <bgroveben@gmail.com>
2016-06-29 23:38:04 -0700

    Build a Webpack configuration file for basics project

--- fundamentals_course/README.md
+++ fundamentals_course/README.md
@@ -7,3 +7,7 @@ http://courses.reactjsprogram.com/courses/reactjsfundamentals
 
 ** In the TOS, Tyler makes it clear that he does not want the content of his paid courses being published by anyone other than him, and I totally agree with that.  
 The code that I have in this repo is from the free (for the price of an email address) React.js Fundamentals course, which I hope doesn't violate the letter or the spirit of the agreement.
+
+
+I am here:  
+http://courses.reactjsprogram.com/courses/reactjsfundamentals/lectures/762538
---
+++ fundamentals_course/some-basics/webpack.config.js
@@ -0,0 +1,24 @@
+/*
+Webpack needs to know three things:
+1) The starting point of your application, or your root JavaScript file.
+2) Which transformations to make on your code.
+3) To which location it should save the newly transformed code.
+*/
+
+module.exports = {
+  // Step 1
+  entry: [
+    './app/index.js'
+  ],
+  // Step 2
+  module: {
+    loaders: [
+      {test: /\.coffee$/, exclude: /node-modules/, loader: "coffee-loader"}
+    ]
+  },
+  // Step 3
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
+}
