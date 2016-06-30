e768d121fad700cdd7f056a3e0eeaf41264b1d9e
bgroveben <bgroveben@gmail.com>
2016-06-30 01:11:39 -0700

    Add some comments and finish up the basics bit

--- fundamentals_course/some-basics/app/index.js
+++ fundamentals_course/some-basics/app/index.js
@@ -1,3 +1,6 @@
+// The first React component of the tutorial.
+// It's here because I don't know where else to put it.
+
 var React = require('react')
 var ReactDOM = require('react-dom')
 var Helloworld = React.createClass({
