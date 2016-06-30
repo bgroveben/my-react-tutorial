15ba3c85657f72daaf1cd5d30d35740d34a69851
bgroveben <bgroveben@gmail.com>
2016-06-30 13:14:31 -0700

    Create user data and some components to display that data

--- fundamentals_course/github-battle/app/index.js
+++ fundamentals_course/github-battle/app/index.js
@@ -1,15 +1,31 @@
+var USER_DATA = {
+  name: 'Benjamin Grove',
+  username: 'bgroveben',
+  image: 'https://avatars0.githubusercontent.com/u/2719094?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function(){
-    return (
-      <div>Hello {this.props.name}</div>
-    )
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
   }
 });
 
+var ProfileLink = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld name="bgroveben" anySortOfData={29}/>,
+  <HelloWorld name="bgroveben" />,
   document.getElementById('app')
 );
