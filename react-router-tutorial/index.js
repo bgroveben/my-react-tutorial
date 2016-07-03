0fe2d027f7e1ea30c8d5ecf78a4a4d3c1ea5a9cb
bgroveben <bgroveben@gmail.com>
2016-07-03 01:51:20 -0700

    Make App the parent of About and Repos components

--- react-router-tutorial/index.js
+++ react-router-tutorial/index.js
@@ -5,10 +5,15 @@ import App from './modules/App';
 import About from './modules/About';
 import Repos from './modules/Repos';
 
+/*
+Nest About and Repos components inside of App so that we can share navigation with all screens in the app
+*/
 render((
   <Router history={hashHistory}>
-    <Route path="/" component={App} />
-    <Route path="/about" component={About} />
-    <Route path="/repos" component={Repos} />
+    <Route path="/" component={App}>
+      {/* Make About and Repos children of App */}
+      <Route path="/about" component={About} />
+      <Route path="/repos" component={Repos} />
+    </Route>
   </Router>
 ), document.getElementById('app'));
