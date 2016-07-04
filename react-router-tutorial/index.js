1d85784a9bb69843799868691628c5f6d37f1dfa
bgroveben <bgroveben@gmail.com>
2016-07-04 02:16:59 -0700

    Import Repo module in index.js file

--- react-router-tutorial/index.js
+++ react-router-tutorial/index.js
@@ -4,6 +4,7 @@ import { Router, Route, hashHistory } from 'react-router';
 import App from './modules/App';
 import About from './modules/About';
 import Repos from './modules/Repos';
+import Repo from './modules/Repo';
 
 /*
 Nest About and Repos components inside of App so that we can share navigation with all screens in the app
