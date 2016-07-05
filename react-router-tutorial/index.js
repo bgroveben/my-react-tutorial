05df0d56ef33009dadc30c39e3d1a1ec6590fa00
bgroveben <bgroveben@gmail.com>
2016-07-05 00:33:51 -0700

    Add a new route, IndexRoute, to index.js and set it to the Home component

--- react-router-tutorial/index.js
+++ react-router-tutorial/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import App from './modules/App';
 import About from './modules/About';
 import Repos from './modules/Repos';
 import Repo from './modules/Repo';
+import Home from './modules/Home';
 
 /*
 Nest About and Repos components inside of App so that we can share navigation with all screens in the app
@@ -12,6 +13,7 @@ Nest About and Repos components inside of App so that we can share navigation wi
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={Home} />
       <Route path="/about" component={About} />
       <Route path="/repos" component={Repos}>
         <Route path="/repos/:userName/:repoName" component={Repo} />
