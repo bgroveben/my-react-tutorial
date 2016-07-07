a8c82c6eb2e9e610e1079ed61b6f32644504cb02
bgroveben <bgroveben@gmail.com>
2016-07-07 07:21:17 -0700

    Create routes file, require dependencies and components

---
+++ fundamentals_course/github-battle/app/config/routes.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var Main = require('../components/Main');
+var Home = require('../components/Home');
