65f825a58026d927c2a797e37b5e7ccb3ed46e2a
bgroveben <bgroveben@gmail.com>
2016-07-07 07:25:16 -0700

    Put some routes in the routes file

--- fundamentals_course/github-battle/app/config/routes.js
+++ fundamentals_course/github-battle/app/config/routes.js
@@ -5,3 +5,13 @@ var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+
+var routes = (
+  <Router>
+    <Route path='/' component={Main}>
+      <Route path='/home' component={Home} />
+    </Route>
+  </Router>
+);
+
+module.exports routes;
