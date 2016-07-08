750f09becb25644365e87aaa64afe95627bfa66d
bgroveben <bgroveben@gmail.com>
2016-07-08 03:15:23 -0700

    Add Continue button to PromptContainer component

--- fundamentals_course/github-battle/app/containers/PromptContainer.js
+++ fundamentals_course/github-battle/app/containers/PromptContainer.js
@@ -13,6 +13,13 @@ var PromptContainer = React.createClass({
                 placeholder="Github Username"
                 type="text" />
             </div>
+            <div className="form-group col-sm-4 col-sm-offset-4">
+              <button
+                className="btn btn-block btn-success"
+                type="submit">
+                  Continue
+              </button>
+            </div>
           </form>
         </div>
       </div>
