18d603542b8fe84f9739718a0ba5632b30d338a1
bgroveben <bgroveben@gmail.com>
2016-07-13 04:37:54 -0700

    Tell ConfirmBattleContainer to change state when player info is loaded

--- fundamentals_course/github-battle/app/containers/ConfirmBattleContainer.js
+++ fundamentals_course/github-battle/app/containers/ConfirmBattleContainer.js
@@ -16,8 +16,11 @@ var ConfirmBattleContainer = React.createClass({
     var query = this.props.location.query;
     githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
       .then(function (players) {
-        console.log('PLAYERS', players)
-      })
+        this.setState({
+          isLoading: false,
+          playerInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
   render: function() {
     return (
